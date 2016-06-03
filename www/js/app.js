d080444c9ebb8193c6b1e2db71e62c89e0db5d38
sumangill <sumandesignfolio@gmail.com>
2016-06-03 09:01:00 +0530

    login page implimented

--- www/js/app.js
+++ www/js/app.js
@@ -32,42 +32,29 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html'
+   
+  })
+  .state('app.home', {
+    url: '/home',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/home.html'
+      }
+    }
+  })
+  .state('app.contactus', {
+    url: '/contactus',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/contactus.html'
       }
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
+  
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('app/login');
 });
